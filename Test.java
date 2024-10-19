98928b6c225cc3c113ec87219cde9233c9cfe7f7
aayushmishra321 <Maayush081@gmail.com>
2024-10-20 00:31:23 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,12 @@
-public class Test {
-    public static void main(String[] args) {
-        int num1 = Integer.parseInt(args[0]);
-        int num2 = Integer.parseInt(args[1]);
-        System.out.println("Addition of " + num1 + " & " + num2 + " is " + (num1 + num2));
-    }
-}
+import sys
+
+# Check if enough arguments are passed
+if len(sys.argv) != 3:
+    print("Please provide two numbers as command-line arguments.")
+else:
+    # Parse command-line arguments
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    
+    # Perform addition and print the result
+    print(f"Addition of {num1} & {num2} is {num1 + num2}")
